ce0e621b81e7a035448cb2b2b62ed22dd95ce7ad
Maher-Khemakhem <khemakhemmaher2003@gmail.com>
2025-02-25 21:57:13 +0100

    last

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { NotificationsComponent } from './views/notifications/notifications.component';
 import { LandingComponent } from './views/landing/landing.component';
 
 export const routes: Routes = [
     {path:'',component:DashboardComponent},
-    {path:'notifications',component:NotificationsComponent},
     {path:'dashboard',component:LandingComponent}
 ];
